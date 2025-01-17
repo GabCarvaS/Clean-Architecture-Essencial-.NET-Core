8330dade19ebd0fe8d1eb1c78536aac837319c33
Gabriel Carvalho <gabrielcfacul@gmail.com>
2025-01-17 16:24:24 -0300

    Implementing JWT authentication

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -4,12 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructureAPI(builder.Configuration);
+builder.Services.AddInfrastructureJWT(builder.Configuration);
+builder.Services.AddInfrastructureSwagger(builder.Configuration);
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -21,6 +22,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStatusCodePages();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
